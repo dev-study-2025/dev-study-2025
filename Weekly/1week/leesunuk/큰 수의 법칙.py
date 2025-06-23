1a749187281da156356988a3f6a94be489b1efd8
leesunuk <leesunook12@naver.com>
2025-06-23 21:06:54 +0900

    [W1/D1]이선욱 ✨ feat: 프로그래머스 두 수의 합 풀이

--- Weekly/1week/leesunuk/큰 수의 법칙.py
+++ Weekly/1week/leesunuk/큰 수의 법칙.py
@@ -1,3 +1,5 @@
+# 풀이
+
 N, M, K = map(int, input().split())
 a = list(map(int, input().split()))
 
@@ -20,4 +22,23 @@ for i in range(M) :
 print(result)
         
 
+# 답안
 
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+#가장 큰 수가 더해지는 횟수 계산
+count = int (m / (k+1)) * k
+count += m % (k+1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second # 두 번쨰로 큰 수 더하기 
+
+print(result) # 최종 답안 출력
