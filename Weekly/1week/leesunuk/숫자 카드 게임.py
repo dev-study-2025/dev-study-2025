67d1ea2e686c5cf15ed34a2837c012d6bb3ad70b
leesunuk <leesunook12@naver.com>
2025-06-24 22:59:51 +0900

    [W1/D2]이선욱 ✨ feat: 숫자 카드 게임 문제 풀이

---
+++ Weekly/1week/leesunuk/숫자 카드 게임.py
@@ -0,0 +1,27 @@
+# 풀이
+
+N, M = map(int, input().split())
+
+result = 0
+
+for _ in range(N) :
+    cards = list(map(int, input().split()))
+    min_value =  min(cards)
+    result = max(result, min_value)
+    
+print(result)
+
+# 또 다른 답안 (이중 for문 활용)
+
+N, M = map(int, input().split())
+
+result = 0
+
+for i in range(N):
+    data = list(map(int, input().split()))
+    min_value = 99999
+    for a in data:
+        min_value = min(min_value, a)
+    result = max(result, min_value)
+
+print(result) 
