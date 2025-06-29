aba23040512b91964f694ed5d5cd943990d9487b
geniusbrainlsm <geniusbrainlsm@gmail.com>
2025-06-29 18:12:11 +0900

    밀린거 커밋갑니다잉

---
+++ .history/Weekly/1week/leesangmin/Day5/Solution_20250629172644.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution {
+
+    public int countTimeWithThree(int N) {
+        int count = 0;
+        for(int i = 0; i<N; i++){
+            if(String.valueOf(i).contains("3")) count++;
+            for(int j = 0; j<=59; j++){
+                if(String.valueOf(j).contains("3")) count++;
+
+                for(int k = 0; k<=59; k++){
+                    if(String.valueOf(k).contains("3")) count++;
+
+                }
+            }
+        }
+        return count; // 일단 0 반환 (구현 후 수정)
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력 예시: 5
+        int N = sc.nextInt();
+
+        Solution s = new Solution();
+        int answer = s.countTimeWithThree(N);
+
+        // 출력 예시: 11475
+        System.out.println(answer);
+    }
+}
